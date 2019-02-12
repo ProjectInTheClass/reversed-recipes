e8563ede491138d38ed3f2a40e27a20821568c0e
GuitarGGun <cgs3966@cau.ac.kr>
2019-02-12 06:13:42 -0800

    update my ingredients

--- sketch_001/sketch_001/ViewController Files/MyRefrigeratorViewController.swift
+++ sketch_001/sketch_001/ViewController Files/MyRefrigeratorViewController.swift
@@ -17,7 +17,9 @@ class MyRefrigeratorViewController: UIViewController {
         Ingredient(name: "돼지고기", icon: "pork", ´class´: .meat, expirationDate: 5),
         Ingredient(name: "우유", icon: "milk", ´class´: .dairyGoods, expirationDate: 3),
         Ingredient(name: "대파", icon: "leek", ´class´: .vegetable, expirationDate: 3),
-        Ingredient(name: "밥", icon: "rice", ´class´: .rice, expirationDate: 3)
+        Ingredient(name: "밥", icon: "rice", ´class´: .rice, expirationDate: 3),
+        Ingredient(name: "김치", icon: "onions", ´class´: .vegetable, expirationDate: 10),
+        Ingredient(name: "마늘", icon: "onions", ´class´: .vegetable, expirationDate: 10)
     ]
     
     let seasonings = [
