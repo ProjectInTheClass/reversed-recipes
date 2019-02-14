f7f831ae56c11c0288a7f5bef4d4c7a037ff5863
chaeunlee <poleewer@gmail.com>
2019-02-15 08:45:31 +0900

    modify icons

--- sketch_001/sketch_001/ViewController Files/FoodListViewController.swift
+++ sketch_001/sketch_001/ViewController Files/FoodListViewController.swift
@@ -149,14 +149,13 @@ class IngredientsListCollectionViewCell: UICollectionViewCell {
             ingredientButton.isSelected = !ingredientButton.isSelected
             ingreStr?.ingredientsList?.append(ingredientName.text!)
             ingredientName.textColor = UIColor.black
-            ingredientImage.tintColor = self.tintColor
-            
+            ingredientImage.image = UIImage(named: "\(ingredientName.text!)")
             
         } else {
 //            print("delete : \(ingredientName.text!)")
             ingredientButton.isSelected = !ingredientButton.isSelected
             ingreStr?.ingredientsList = ingreStr?.ingredientsList?.filter { $0 != ingredientName.text! }
-            ingredientImage.tintColor = UIColor.lightGray
+            ingredientImage.image = UIImage(named: "\(ingredientName.text!)" + "_gray")
             ingredientName.textColor = UIColor.lightGray
             
         }
